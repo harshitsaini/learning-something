f334b2603fc5e776e68ef4ffc719899397c23f97
Harshit Saini <harshit.saini@widas.in>
2019-12-05 20:12:10 +0530

    added some bash scripts

---
+++ learning_bash/loop_test.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# chmod u+x ./loop_test.sh
+for i in {1..5}
+do
+   echo "Welcome $i times"
+done
+
+
+#!/bin/bash
+echo "Bash version ${BASH_VERSION}..."
+for i in {0..10..2}
+  do 
+     echo "Welcome $i times"
+ done
+
+
+for (( c=1; c<=5; c++ ))
+do  
+   echo "Welcome $c times"
+done
