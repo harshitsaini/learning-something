e622e372713e0f1b50cd025e9e3719213aebb936
Harshit Saini <harshit.saini@widas.in>
2020-02-26 16:09:26 +0530

    argparse update

--- learning_argparse/test1.py
+++ learning_argparse/test1.py
@@ -1,8 +1,8 @@
-# import argparse
-# parser = argparse.ArgumentParser()
-# parser.add_argument("--square", help="display a square of a given number", type=int)
-# args = parser.parse_args()
-# print(args.square**2)
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("--square", help="display a square of a given number", type=int)
+args = parser.parse_args()
+print(args.square**2)
 
 
 # import argparse
@@ -140,23 +140,23 @@
 #     print("{}^{} == {}".format(args.x, args.y, answer))
 
 
-import argparse
+# import argparse
 
-parser = argparse.ArgumentParser(description="calculate X to the power of Y")
-group = parser.add_mutually_exclusive_group()
-group.add_argument("-v", "--verbose", action="store_true")
-group.add_argument("-q", "--quiet", action="store_true")
-parser.add_argument("x", type=int, help="the base")
-parser.add_argument("y", type=int, help="the exponent")
-args = parser.parse_args()
-answer = args.x**args.y
+# parser = argparse.ArgumentParser(description="calculate X to the power of Y")
+# group = parser.add_mutually_exclusive_group()
+# group.add_argument("-v", "--verbose", action="store_true")
+# group.add_argument("-q", "--quiet", action="store_true")
+# parser.add_argument("x", type=int, help="the base")
+# parser.add_argument("y", type=int, help="the exponent")
+# args = parser.parse_args()
+# answer = args.x**args.y
 
-if args.quiet:
-    print(answer)
-elif args.verbose:
-    print("{} to the power {} equals {}".format(args.x, args.y, answer))
-else:
-    print("{}^{} == {}".format(args.x, args.y, answer))
+# if args.quiet:
+#     print(answer)
+# elif args.verbose:
+#     print("{} to the power {} equals {}".format(args.x, args.y, answer))
+# else:
+#     print("{}^{} == {}".format(args.x, args.y, answer))
 
 
 
